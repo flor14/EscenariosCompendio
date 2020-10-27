c6ff7c101c76ec8925e1fc38a5e8c2414a66f662
Florencia <florencia.dandrea@gmail.com>
2020-10-27 15:26:21 -0300

    paquete

---
+++ script_maestro.R
@@ -0,0 +1,10 @@
+# install.packages("devtools")
+#devtools::install_github("benmarwick/rrtools")
+library(rrtools)
+
+
+# como yo ya genere el proyecto esta funcion genera
+# la estructura de paquete
+# IMPORTANTE! si ya cree el proyecto incluir el path
+rrtools::use_compendium("C:/Users/FLORENCIA/Dropbox/paper_escenarios/EscenariosCompendio")
+
